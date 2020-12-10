f68057c924f37888e1bad2b18380e6e5243de961
Scott Semtner <sjsemtner@gmail.com>
2020-12-10 08:52:53 -0800

    added set group function to admin

--- BallotMeasures/admin.py
+++ BallotMeasures/admin.py
@@ -6,10 +6,26 @@ def reset_likes(queryset):
     queryset.update(likes=0)
 
 
-class GroupAdmin(admin.ModelAdmin):
+def change_group_jaguars(querryset):
+    for i in querryset:
+        i.group.class_name = "Jaguars"
+        i.save()
+
+
+def change_group_iguanas(querryset):
+    for i in querryset:
+        i.group.class_name = "Iguanas"
+        i.save()
+
+
+class LikesAdmin(admin.ModelAdmin):
     actions = [reset_likes]
 
 
+class StudentAdmin(admin.ModelAdmin):
+    actions = [change_group_jaguars, change_group_iguanas]
+
+
 admin.site.register(Topic)
-admin.site.register(Group, GroupAdmin)
+admin.site.register(Group, LikesAdmin)
 admin.site.register(Student)
--- Coffee/admin.py
+++ Coffee/admin.py
@@ -6,8 +6,24 @@ def reset_likes(queryset):
     queryset.update(likes=0)
 
 
+def change_group_jaguars(querryset):
+    for i in querryset:
+        i.group.class_name = "Jaguars"
+        i.save()
+
+
+def change_group_iguanas(querryset):
+    for i in querryset:
+        i.group.class_name = "Iguanas"
+        i.save()
+
+
 class LikesAdmin(admin.ModelAdmin):
     actions = [reset_likes]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    actions = [change_group_jaguars, change_group_iguanas]
 
 
 admin.site.register(Artwork, LikesAdmin)
