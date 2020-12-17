96b2316dd377fe1e45873150a2fbb782caa6e6e0
Scott Semtner <sjsemtner@gmail.com>
2020-12-17 12:26:23 -0800

    added secret file

---
+++ CheckYoSelf/secrets.py
@@ -0,0 +1,1 @@
+DJANGO_SECRET = '(9_(y+l(=$e27-m*4=6e2r_6p*5gyqsvguk9%oi@=6veh3&n_9'
--- CheckYoSelf/settings.py
+++ CheckYoSelf/settings.py
@@ -15,13 +15,15 @@ from pathlib import Path
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from CheckYoSelf.secrets import DJANGO_SECRET
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(9_(y+l(=$e27-m*4=6e2r_6p*5gyqsvguk9%oi@=6veh3&n_9'
+SECRET_KEY = DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
