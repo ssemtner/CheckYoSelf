36b497df3cd87af03b77476d393b56d3ba436edc
Scott Semtner <sjsemtner@gmail.com>
2021-01-13 10:23:03 -0800

    Added element number

--- Elements/models.py
+++ Elements/models.py
@@ -4,6 +4,7 @@ from tinymce.models import HTMLField
 
 class Product(models.Model):
     element = models.CharField(max_length=50, unique=True)
+    number = models.IntegerField(unique=True)
     author = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     body = HTMLField()
--- Elements/views.py
+++ Elements/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 # Create your views here.
 def index(request):
-    element_list = Product.objects.order_by('element')
+    element_list = Product.objects.order_by('number')
     return render(request, 'Elements/index.html', {'element_list': element_list})
 
 
