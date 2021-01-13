d26b3fe22e8f57fbebb610f261917c6524a5bce4
Scott Semtner <sjsemtner@gmail.com>
2021-01-13 09:03:58 -0800

    Finished elements index page

--- Elements/views.py
+++ Elements/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Product
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'Elements/index.html')
+    element_list = Product.objects.order_by('element')
+    return render(request, 'Elements/index.html', {'element_list': element_list})
